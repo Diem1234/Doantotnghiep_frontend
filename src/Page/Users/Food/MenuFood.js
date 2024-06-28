41161ffb74192e53ea98a2eb6ad1c383e8554fe7
Diem1234 <diemnguyen.16032002dmx@gmail.com>
2024-06-28 15:59:57 +0700

    'u'

--- src/Page/Users/Food/MenuFood.js
+++ src/Page/Users/Food/MenuFood.js
@@ -6,7 +6,7 @@ import { Checkbox, Radio } from "antd";
 import { Prices } from "../../../components/Routes/Prices";
 import ReactPaginate from "react-paginate";
 
-const FoodItem = memo(({ food }) => {
+const FoodItem = ({ food }) => {
   const navigate = useNavigate();
 
   return (
@@ -30,21 +30,17 @@ const FoodItem = memo(({ food }) => {
       </div>
     </div>
   );
-});
+};
 
 const MenuFood = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const [categories, setCategories] = useState([]);
-  const [filters, setFilters] = useState({
-    checked: [],
-    radio: [],
-  });
-
+  const [checked, setChecked] = useState([]);
+  const [radio, setRadio] = useState([]);
   const [food, setFood] = useState([]);
-
   const [noFoodsFound, setNoFoodsFound] = useState(false);
-  const [selected,setSelected] = useState(null)
+  const [selected, setSelected] = useState(null);
   const itemsPerPage = 6; // Số mục trên mỗi trang
   const [currentPage, setCurrentPage] = useState(0);
 
@@ -54,7 +50,7 @@ const MenuFood = () => {
 
   // Tính toán dữ liệu hiện tại cho trang
   const offset = currentPage * itemsPerPage;
-  const currentFood = useMemo(() => food.slice(offset, offset + itemsPerPage), [food, offset, itemsPerPage]);
+  const currentFood = food.slice(offset, offset + itemsPerPage);
   const pageCount = Math.ceil(food.length / itemsPerPage);
 
   const { setTitle } = useTitle();
@@ -65,9 +61,8 @@ const MenuFood = () => {
   useEffect(() => {
     getAllCategories();
   }, []);
-  const { categoryId } = useParams()
+  const { categoryId } = useParams();
 
-    
   const getAllCategories = async () => {
     try {
       const response = await axiosClient.get("api/v1/categories");
@@ -81,76 +76,59 @@ const MenuFood = () => {
     }
   };
 
-  const handleFilter = (value, id) => {
-    setFilters((prevFilters) => ({
-      ...prevFilters,
-      checked: value
-        ? [...prevFilters.checked, id]
-        : prevFilters.checked.filter((c) => c !== id),
-    }));
-  };
-
-  const handlePriceFilter = (value) => {
-    setFilters((prevFilters) => ({
-      ...prevFilters,
-      radio: value,
-    }));
-  };
-
-  // Memoize hàm fetchFoodsByCategory
-  const fetchFoodsByCategory = useCallback(async () => {
+  const getAllFoods = async () => {
     try {
-      const response = await axiosClient.get(`api/v1/food/foods/category/${categoryId}`);
-      setFood(response.data.payload);
-      setNoFoodsFound(response.data.payload.length === 0);
+      const response = await axiosClient.get("api/v1/food/");
+      console.log(response);
+      if (response) {
+        setFood(response.data.payload.slice(0, 8));
+      } else {
+        alert("khong co du lieu!");
+      }
     } catch (error) {
       console.error(error);
     }
-  }, [categoryId]);
+  };
+  //filter by cat
+  const handleFilter = (value, id) => {
+    let all = [...checked];
+    if (value) {
+      if (!all.includes(id)) {
+        all.push(id);
+      }
+    } else {
+      all = all.filter((c) => c !== id);
+    }
+    setChecked(all);
+  };
+  useEffect(() => {
+    if (!checked.length || !radio.length || !categoryId) getAllFoods();
+  }, [checked.length, radio.length]);
 
-  // Memoize hàm fetchFood
-  const fetchFood = useCallback(async () => {
+  useEffect(() => {
+    console.log("check", checked, radio);
+    
+    if (checked.length || radio.length || categoryId) filterFood();
+  }, [checked, radio, categoryId]);
 
+  //get filter product
+  const filterFood = async (e) => {
     try {
-      const { data } = await axiosClient.post('api/v1/food/food-filters', {
-        checked: filters.checked,
-        radio: filters.radio,
-      });
-      setFood(data.payload);
-      setNoFoodsFound(data.payload.length === 0);
+      const response = await axiosClient.get(
+        `api/v1/food/foods/cate/${!categoryId ? "" : categoryId}`,
+        {
+          checked: checked.length > 0 ? checked : [],
+          radio: radio.length > 0 ? radio : [],
+        }
+      );
+      console.log("req.params.categoryId:", categoryId);
+      console.log("data:", response.data);
+      setFood(response.data?.payload);
+      setNoFoodsFound(response.data?.payload.length === 0);
     } catch (error) {
       console.log(error);
     }
-   
-  }, [filters]);
-
-  // Cập nhật URL khi thay đổi bộ lọc
-  useEffect(() => {
-    const searchParams = new URLSearchParams(location.search);
-    filters.checked.forEach((f) => searchParams.append('checked', f));
-    searchParams.set('radio', filters.radio);
-    navigate(`${location.pathname}?${searchParams.toString()}`, { replace: true });
-  }, [filters, location, navigate]);
-
-  // Lấy danh sách món ăn khi thay đổi categoryId hoặc filters
-  useEffect(() => {
-    fetchFoodsByCategory();
-  }, [categoryId, fetchFoodsByCategory]);
-
-  useEffect(() => {
-    fetchFood();
-  }, [filters, fetchFood]);
-
-  // Khôi phục bộ lọc từ URL
-  useEffect(() => {
-    const searchParams = new URLSearchParams(location.search);
-    const checkedParams = searchParams.getAll('checked');
-    const radioParam = searchParams.get('radio');
-    setFilters({ checked: checkedParams, radio: radioParam || '' });
-  }, [location.search]);
-
-
-  
+  };
 
   return (
     <div className="container-xxl py-5">
@@ -170,7 +148,7 @@ const MenuFood = () => {
             </div>
             <h4 className="text-center mt-4">Lọc theo giá</h4>
             <div className="d-flex flex-column mb-5">
-              <Radio.Group onChange={(e) => handlePriceFilter(e.target.value)}>
+              <Radio.Group onChange={(e) => setRadio(e.target.value)}>
                 {Prices?.map((p) => (
                   <div key={p._id}>
                     <Radio value={p.array}>{p.name}</Radio>
@@ -189,43 +167,41 @@ const MenuFood = () => {
           </div>
           <div className="col-md-9">
             <div className="d-flex flex-wrap">
-            {noFoodsFound ? (
-              <div className="text-center my-5">
-                <h3>Không tìm thấy món ăn nào</h3>
-                <p>Hãy thử thay đổi bộ lọc để tìm kiếm</p>
-              </div>
-            ) : (
-              <div className="d-flex flex-wrap">
-                {currentFood.map((p,index) => (
-                  <FoodItem key={index} food={p} />
-                ))}
-              </div>
-            )}
+              {noFoodsFound ? (
+                <div className="text-center my-5">
+                  <h3>Không tìm thấy món ăn nào</h3>
+                  <p>Hãy thử thay đổi bộ lọc để tìm kiếm</p>
+                </div>
+              ) : (
+                <div className="d-flex flex-wrap">
+                  {currentFood.map((p, index) => (
+                    <FoodItem key={index} food={p} />
+                  ))}
+                </div>
+              )}
             </div>
             <div className="m-2 p-3">
-           
-                <nav aria-label="Page navigation example ">
-                  <ReactPaginate
-                    previousLabel={"«"}
-                    nextLabel={"»"}
-                    breakLabel={"..."}
-                    breakClassName={"break-me"}
-                    pageCount={pageCount}
-                    marginPagesDisplayed={2}
-                    pageRangeDisplayed={5}
-                    onPageChange={handlePageClick}
-                    containerClassName={"pagination"}
-                    subContainerClassName={"pages pagination-sm"}
-                    activeClassName={"active"}
-                    pageLinkClassName={"page-link"}
-                    previousLinkClassName={"page-link"}
-                    nextLinkClassName={"page-link"}
-                    pageClassName={"page-item"}
-                    previousClassName={"page-item"}
-                    nextClassName={"page-item"}
-                  />
-                </nav>
-              
+              <nav aria-label="Page navigation example ">
+                <ReactPaginate
+                  previousLabel={"«"}
+                  nextLabel={"»"}
+                  breakLabel={"..."}
+                  breakClassName={"break-me"}
+                  pageCount={pageCount}
+                  marginPagesDisplayed={2}
+                  pageRangeDisplayed={5}
+                  onPageChange={handlePageClick}
+                  containerClassName={"pagination"}
+                  subContainerClassName={"pages pagination-sm"}
+                  activeClassName={"active"}
+                  pageLinkClassName={"page-link"}
+                  previousLinkClassName={"page-link"}
+                  nextLinkClassName={"page-link"}
+                  pageClassName={"page-item"}
+                  previousClassName={"page-item"}
+                  nextClassName={"page-item"}
+                />
+              </nav>
             </div>
           </div>
         </div>
